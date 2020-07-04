96faf84cd5271560acbb45dc72f1d381b426d5e8
tabrezakhtar <tabrez.akhtar1@gmail.com>
2020-07-04 15:43:16 +0100

    change background colour on breakpoints

--- src/App.js
+++ src/App.js
@@ -4,32 +4,24 @@ import './App.scss';
 
 const App = () => (
   <div className="container">
-    <Router>
+
       <header className="header">
         <span className="header__logo">Header</span>
 
           <ul className="header__menu">
-            <li className="menu__item"><Link to="/">2x2</Link></li>
-            <li className="menu__item"><Link to="/three">3x2</Link></li>
-            <li className="menu__item"><Link to="/one">1x1</Link></li>
-            <li className="menu__item"><Link to="/side">with sidebar</Link></li>
-            <li className="menu__item"><Link to="/forms">forms</Link></li>
+            <li className="menu__item">One</li>
+            <li className="menu__item">Two</li>
+            <li className="menu__item">Three</li>
+            <li className="menu__item">Four</li>
+            <li className="menu__item">Five</li>
           </ul>
-        
       </header>
 
-      <main>
-        <Switch>
-          <Route exact path="/"><TwoByTwo /></Route>
-          <Route exact path="/three"><ThreeByTwo /></Route>
-          <Route exact path="/one"><OneByOne> /></OneByOne></Route>
-          <Route exact path="/side"><Side /></Route>
-          <Route exact path="/forms"><Forms /></Route>
-        </Switch>
-      </main>
+      <div className="main">
+        main
+      </div>
 
       <footer>Footer</footer>
-    </Router>
   </div>
 );
 
